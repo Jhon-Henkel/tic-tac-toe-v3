e7ef10f71de7da094174a71ec4868401988a6313
Jhon-climba <jhowrf@gmail.com>
2022-02-28 13:43:22 -0300

    Update
    Correção de bugs, melhorado código para ficar menos repetitivo, melhorias visuais e criado mais funções para facilitar manutenção no código.

--- index.php
+++ index.php
@@ -7,20 +7,41 @@
     <title>Jogo da velha</title>
 </head>
 
-<body>
-<div>
-    <h1>Jogo da velha</h1>
+<body class="conteudo-body">
+<div class="conteudo">
+    <h1>
+        <span class="x"><b>--X</b></span>
+            <span class="o"><b>O </b></span>
+                Jogo da velha
+            <span class="o"><b> O</b></span>
+        <span class="x"><b>X--</b></span>
+    </h1>
     <form class="center" method="post" action="main.php">
-        </br>
-        <label>
-            <input type="radio" name="player" value="1" checked> Single Player
-            <input type="radio" name="player" value="2"> Two Player's </br></br>
-            <br><b>Dificuldade da IA no single player: </b></br></br>
-            <input type="radio" name="dificuldade" value="1" title="Não irei me defender."> Burra
-            <input type="radio" name="dificuldade" value="2" title="Irei me defender, mas pegarei leve."> Leve
-            <input type="radio" name="dificuldade" value="3" title="Não irei deixar você ganhar!!! " checked> Pesada </br></br>
-            <input type="submit" value="Bora Jogar!">
-        </label>
+        <fieldset class="inicio">
+            <legend>Como irá jogar?</legend>
+            <input type="radio" name="player" id="1" value="1" checked><label for="1">Sozinho</label>
+            <input type="radio" name="player" id="2" value="2"><label for="2">Acompanhado</label><br>
+        </fieldset><br>
+
+        <fieldset class="inicio">
+            <legend>Dificuldade modo solitário:</legend>
+            <input type="radio" name="dificuldade" id="3" value="1"><label for="3" title="Para crianças.">Extra-fácil</label>
+            <input type="radio" name="dificuldade" id="4" value="2"><label for="4" title="Para quem tem cérebro pequeno.">Fácil</label>
+            <input type="radio" name="dificuldade" id="5" value="3" checked><label for="5" title="Para quem sabe pensar.">Média</label>
+        </fieldset><br>
+
+        <fieldset class="inicio">
+            <legend>Quem começa?</legend>
+            <input type="radio" name="quem_joga" id="6" value="1"><label for="6">X</label>
+            <input type="radio" name="quem_joga" id="7" value="2"><label for="7">O</label>
+            <input type="radio" name="quem_joga" id="8" value="3" checked><label for="8">Tanto faz!!!</label>
+        </fieldset>
+
+        <span class="x"><b>--X</b></span>
+            <span class="o"><b>O</b></span>
+                <input class="btn_start" type="submit" value="Bora Jogar!">
+            <span class="o"><b>O</b></span>
+        <span class="x"><b>X--</b></span>
     </form>
 </div>
 </body>
--- main.php
+++ main.php
@@ -10,10 +10,8 @@
 <body class="conteudo-body">
 <div class="conteudo">
     <?php
-        include "functions.php";
-
+        require_once "functions.php";
         $player = $_POST["player"] ?? null;//lê o valor informado na tela index.php
-
         if ($player == 1){
             $_SESSION['player'] = 1;
         }elseif($player == 2){
@@ -21,315 +19,85 @@
         }//coloca o valor passado pela tela home em uma variavel global.
 
         $_SESSION ['tabuleiro'] = $_SESSION ['tabuleiro'] ?? null;
-
         if ($_SESSION ['tabuleiro'] == null) {
             $_SESSION ['j'] = [array(1, 2, 3), array(4, 5, 6), array(7, 8, 9)];
+            $_SESSION ['tabuleiro'] = true;
         }//Cria o Array do tabuleiro e numera as casas.
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         //////////////////////////////////////////////////ONE PLAYER////////////////////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
         if ($_SESSION ['player'] == 1){
-            $_SESSION ['tabuleiro'] = true;
-            $x = $_POST['X'] ?? null; //pega o valor digitado por 'X' no formulário.
-            $_SESSION['deu_velha'] = $_SESSION['deu_velha'] ?? null;
-            $_SESSION['x/o'] = $_SESSION['x/o'] ?? 1;//variável que faz a troca dos formulários de jogada.
-            $_SESSION['ia'] = $_SESSION['ia'] ?? 3; //variável responsável por ativar as jogadas de O da IA.
+            $x = $_POST['X'] ?? null; //pega o valor escolhido por 'X' no formulário.
             $dificuldade = $_POST['dificuldade'] ?? null; //dificuldade escolhida, padrão é 3 (difícil).
+            quem_inicia(); //define quem inicia o jogo.
 
             if ($dificuldade == 1){
                 $_SESSION ['dif'] = 1;
-                $_SESSION ['dificuldade'] = "Burra";
+                $_SESSION ['dificuldade'] = "Extra-Fácil.";
             }elseif ($dificuldade == 2) {
                 $_SESSION ['dif'] = 2;
-                $_SESSION ['dificuldade'] = "Leve";
+                $_SESSION ['dificuldade'] = "Fácil.";
             }elseif ($dificuldade == 3) {
                 $_SESSION ['dif'] = 3;
-                $_SESSION ['dificuldade'] = "Pesada";
+                $_SESSION ['dificuldade'] = "Média";
             } // armazena a dificuldade escolhida em uma variavel global e dá nome.
+            echo "<b>Dificuldade da IA: </b>"."<span class='vermelho'>".$_SESSION['dificuldade']."</span></br></br>";
 
-            echo "<b>Dificuldade da IA: </b>"."<span class='dificuldade'>".$_SESSION['dificuldade']."</span></br></br>";
-
-            if ($_SESSION['x/o'] == 1) {
-                if ($x == 1) {
-                    if ($_SESSION['j'][0][0] == '<span class="x">X</span>' || $_SESSION['j'][0][0] == '<span class="o">O</span>'){
-                        jogada_invalida();
-                    }elseif ($_SESSION['j'][0][0] == 1) {
-                        $_SESSION['j'][0][0] = '<span class="x">X</span>';
-                        $_SESSION['deu_velha']++;
-                        $_SESSION['ia'] = 1;
-                        $_SESSION['x/o'] = 3;
-                    }
-                }elseif ($x == 2){
-                    if ($_SESSION['j'][0][1] == '<span class="x">X</span>' || $_SESSION['j'][0][1] == '<span class="o">O</span>'){
-                        jogada_invalida();
-                    }elseif ($_SESSION['j'][0][1] == 2) {
-                        $_SESSION['j'][0][1] = '<span class="x">X</span>';
-                        $_SESSION['deu_velha']++;
-                        $_SESSION['ia'] = 1;
-                        $_SESSION['x/o'] = 3;
-                    }
-                }elseif ($x == 3){
-                    if ($_SESSION['j'][0][2] == '<span class="x">X</span>' || $_SESSION['j'][0][2] == '<span class="o">O</span>'){
-                        jogada_invalida();
-                    }elseif ($_SESSION['j'][0][2] == 3) {
-                        $_SESSION['j'][0][2] = '<span class="x">X</span>';
-                        $_SESSION['deu_velha']++;
-                        $_SESSION['ia'] = 1;
-                        $_SESSION['x/o'] = 3;
-                    }
-                }elseif ($x == 4){
-                    if ($_SESSION['j'][1][0] == '<span class="x">X</span>' || $_SESSION['j'][1][0] == '<span class="o">O</span>'){
-                        jogada_invalida();
-                    }elseif ($_SESSION['j'][1][0] == 4) {
-                        $_SESSION['j'][1][0] = '<span class="x">X</span>';
-                        $_SESSION['deu_velha']++;
-                        $_SESSION['ia'] = 1;
-                        $_SESSION['x/o'] = 3;
-                    }
-                }elseif ($x == 5){
-                    if ($_SESSION['j'][1][1] == '<span class="x">X</span>' || $_SESSION['j'][1][1] == '<span class="o">O</span>'){
-                        jogada_invalida();
-                    }elseif ($_SESSION['j'][1][1] == 5) {
-                        $_SESSION['j'][1][1] = '<span class="x">X</span>';
-                        $_SESSION['deu_velha']++;
-                        $_SESSION['ia'] = 1;
-                        $_SESSION['x/o'] = 3;
-                    }
-                }elseif ($x == 6){
-                    if ($_SESSION['j'][1][2] == '<span class="x">X</span>' || $_SESSION['j'][1][2] == '<span class="o">O</span>'){
-                        jogada_invalida();
-                    }elseif ($_SESSION['j'][1][2] == 6) {
-                        $_SESSION['j'][1][2] = '<span class="x">X</span>';
-                        $_SESSION['deu_velha']++;
-                        $_SESSION['ia'] = 1;
-                        $_SESSION['x/o'] = 3;
-                    }
-                }elseif ($x == 7){
-                    if ($_SESSION['j'][2][0] == '<span class="x">X</span>' || $_SESSION['j'][2][0] == '<span class="o">O</span>'){
-                        jogada_invalida();
-                    }elseif ($_SESSION['j'][2][0] == 7) {
-                        $_SESSION['j'][2][0] = '<span class="x">X</span>';
-                        $_SESSION['deu_velha']++;
-                        $_SESSION['ia'] = 1;
-                        $_SESSION['x/o'] = 3;
-                    }
-                }elseif ($x == 8){
-                    if ($_SESSION['j'][2][1] == '<span class="x">X</span>' || $_SESSION['j'][2][1] == '<span class="o">O</span>'){
-                        jogada_invalida();
-                    }elseif ($_SESSION['j'][2][1] == 8) {
-                        $_SESSION['j'][2][1] = '<span class="x">X</span>';
-                        $_SESSION['deu_velha']++;
-                        $_SESSION['ia'] = 1;
-                        $_SESSION['x/o'] = 3;
-                    }
-                }elseif ($x == 9){
-                    if ($_SESSION['j'][2][2] == '<span class="x">X</span>' || $_SESSION['j'][2][2] == '<span class="o">O</span>'){
-                        jogada_invalida();
-                    }elseif ($_SESSION['j'][2][2] == 9) {
-                        $_SESSION['j'][2][2] = '<span class="x">X</span>';
-                        $_SESSION['deu_velha']++;
-                        $_SESSION['ia'] = 1;
-                        $_SESSION['x/o'] = 3;
+            for ($jxo=1; $jxo<=9; $jxo++) {
+                for ($jc = 0; $jc <= 2; $jc++) {
+                    for ($jl = 0; $jl <= 2; $jl++) {
+                        if ($x == $jxo && $_SESSION['j'][$jc][$jl] == $jxo) {
+                            $_SESSION['j'][$jc][$jl] = '<span class="x">X</span>';
+                            $_SESSION['deu_velha']++;
+                            $_SESSION['x/o'] = false;
+                            $_SESSION['ia'] = 1;
+                            $x = null;
+                            $o = null;
+                        }
                     }
                 }
             }//jogadas de 'X'.
-
+            jogada_invalida($x, null);//faz a validação de jogada.
             jogada_ia ();//faz a jogada de O pela IA.
             fim_jogo ();//valida o fim da partida.
             jogadas_x_o();//informa as jogadas de X e O.
-
         }//jogadas one player.
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         //////////////////////////////////////////////////TWO PLAYER////////////////////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         if ($_SESSION ['player'] == 2){
-            $_SESSION ['tabuleiro'] = true;
-            $x = $_POST['X'] ?? null; //pega o valor digitado por 'X' no formulário.
-            $o = $_POST['O'] ?? null; //pega o valor digitado por 'O' no formulário.
-            $_SESSION['deu_velha'] = $_SESSION['deu_velha'] ?? null;
-            $_SESSION['x/o'] = $_SESSION['x/o'] ?? 1;//variável que faz a troca dos formulários de jogada.
+            $x = $_POST['X'] ?? null; //pega o valor escolhido por 'X' no formulário.
+            $o = $_POST['O'] ?? null; //pega o valor escolhido por 'O' no formulário.
+            quem_inicia(); //define quem inicia o jogo.
 
-            /*for ($jx=1; $jx<9; $jx++){
-                if ($x == $jx) {
-                    for ($jc=0; $jc<2; $jc++){
-                        for ($jl=0; $jl<2; $jl++) {
-                            if ($_SESSION['j'][$jc][$jl] == '<span class="x">X</span>' || $_SESSION['j'][$jc][$jl] == '<span class="o">O</span>') {
-                                jogada_invalida();
-                            } elseif ($_SESSION['j'][$jc][$jl] == $jx) {
-                                $_SESSION['j'][$jc][$jl] = '<span class="x">X</span>';
-                                $_SESSION['deu_velha']++;
-                                $_SESSION['x/o'] = 2;
-                            }
+            for ($jxo=1; $jxo<=9; $jxo++){
+                for ($jc=0; $jc<=2; $jc++){
+                    for ($jl=0; $jl<=2; $jl++){
+                        if ($x == $jxo && $_SESSION['j'][$jc][$jl] == $jxo){
+                            $_SESSION['j'][$jc][$jl] = '<span class="x">X</span>';
+                            $_SESSION['deu_velha']++;
+                            $_SESSION['x/o'] = 2;
+                            $x = null;
+                            $o = null;
+                        }elseif ($o == $jxo && $_SESSION['j'][$jc][$jl] == $jxo){
+                            $_SESSION['j'][$jc][$jl] = '<span class="o">O</span>';
+                            $_SESSION['deu_velha']++;
+                            $_SESSION['x/o'] = 1;
+                            $x = null;
+                            $o = null;
                         }
                     }
                 }
-            }
-            echo 'jc = '.$jc.' jl = '.$jl.' jx= '.$jx;*/
-
-            if ($x == 1) {
-                if ($_SESSION['j'][0][0] == '<span class="x">X</span>' || $_SESSION['j'][0][0] == '<span class="o">O</span>'){
-                    jogada_invalida();
-                }elseif ($_SESSION['j'][0][0] == 1) {
-                    $_SESSION['j'][0][0] = '<span class="x">X</span>';
-                    $_SESSION['deu_velha']++;
-                    $_SESSION['x/o'] = 2;
-                }
-            }elseif ($x == 2){
-                if ($_SESSION['j'][0][1] == '<span class="x">X</span>' || $_SESSION['j'][0][1] == '<span class="o">O</span>'){
-                    jogada_invalida();
-                }elseif ($_SESSION['j'][0][1] == 2) {
-                    $_SESSION['j'][0][1] = '<span class="x">X</span>';
-                    $_SESSION['deu_velha']++;
-                    $_SESSION['x/o'] = 2;
-                }
-            }elseif ($x == 3){
-                if ($_SESSION['j'][0][2] == '<span class="x">X</span>' || $_SESSION['j'][0][2] == '<span class="o">O</span>'){
-                    jogada_invalida();
-                }elseif ($_SESSION['j'][0][2] == 3) {
-                    $_SESSION['j'][0][2] = '<span class="x">X</span>';
-                    $_SESSION['deu_velha']++;
-                    $_SESSION['x/o'] = 2;
-                }
-            }elseif ($x == 4){
-                if ($_SESSION['j'][1][0] == '<span class="x">X</span>' || $_SESSION['j'][1][0] == '<span class="o">O</span>'){
-                    jogada_invalida();
-                }elseif ($_SESSION['j'][1][0] == 4) {
-                    $_SESSION['j'][1][0] = '<span class="x">X</span>';
-                    $_SESSION['deu_velha']++;
-                    $_SESSION['x/o'] = 2;
-                }
-            }elseif ($x == 5){
-                if ($_SESSION['j'][1][1] == '<span class="x">X</span>' || $_SESSION['j'][1][1] == '<span class="o">O</span>'){
-                    jogada_invalida();
-                }elseif ($_SESSION['j'][1][1] == 5) {
-                    $_SESSION['j'][1][1] = '<span class="x">X</span>';
-                    $_SESSION['deu_velha']++;
-                    $_SESSION['x/o'] = 2;
-                }
-            }elseif ($x == 6){
-                if ($_SESSION['j'][1][2] == '<span class="x">X</span>' || $_SESSION['j'][1][2] == '<span class="o">O</span>'){
-                    jogada_invalida();
-                }elseif ($_SESSION['j'][1][2] == 6) {
-                    $_SESSION['j'][1][2] = '<span class="x">X</span>';
-                    $_SESSION['deu_velha']++;
-                    $_SESSION['x/o'] = 2;
-                }
-            }elseif ($x == 7){
-                if ($_SESSION['j'][2][0] == '<span class="x">X</span>' || $_SESSION['j'][2][0] == '<span class="o">O</span>'){
-                    jogada_invalida();
-                }elseif ($_SESSION['j'][2][0] == 7) {
-                    $_SESSION['j'][2][0] = '<span class="x">X</span>';
-                    $_SESSION['deu_velha']++;
-                    $_SESSION['x/o'] = 2;
-                }
-            }elseif ($x == 8){
-                if ($_SESSION['j'][2][1] == '<span class="x">X</span>' || $_SESSION['j'][2][1] == '<span class="o">O</span>'){
-                    jogada_invalida();
-                }elseif ($_SESSION['j'][2][1] == 8) {
-                    $_SESSION['j'][2][1] = '<span class="x">X</span>';
-                    $_SESSION['deu_velha']++;
-                    $_SESSION['x/o'] = 2;
-                }
-            }elseif ($x == 9){
-                if ($_SESSION['j'][2][2] == '<span class="x">X</span>' || $_SESSION['j'][2][2] == '<span class="o">O</span>'){
-                    jogada_invalida();
-                }elseif ($_SESSION['j'][2][2] == 9) {
-                    $_SESSION['j'][2][2] = '<span class="x">X</span>';
-                    $_SESSION['deu_velha']++;
-                    $_SESSION['x/o'] = 2;
-                }
-            }//jogadas de 'X'.
-
-            if ($o == 1){
-                if ($_SESSION['j'][0][0] == '<span class="x">X</span>' || $_SESSION['j'][0][0] == '<span class="o">O</span>'){
-                    jogada_invalida();
-                }elseif ($_SESSION['j'][0][0] == 1) {
-                    $_SESSION['j'][0][0] = '<span class="o">O</span>';
-                    $_SESSION['deu_velha']++;
-                    $_SESSION['x/o'] = 1;
-                }
-            }elseif ($o == 2){
-                if ($_SESSION['j'][0][1] == '<span class="x">X</span>' || $_SESSION['j'][0][1] == '<span class="o">O</span>'){
-                    jogada_invalida();
-                }elseif ($_SESSION['j'][0][1] == 2) {
-                    $_SESSION['j'][0][1] = '<span class="o">O</span>';
-                    $_SESSION['deu_velha']++;
-                    $_SESSION['x/o'] = 1;
-                }
-            }elseif ($o == 3){
-                if ($_SESSION['j'][0][2] == '<span class="x">X</span>' || $_SESSION['j'][0][2] == '<span class="o">O</span>'){
-                    jogada_invalida();
-                }elseif ($_SESSION['j'][0][2] == 3) {
-                    $_SESSION['j'][0][2] = '<span class="o">O</span>';
-                    $_SESSION['deu_velha']++;
-                    $_SESSION['x/o'] = 1;
-                }
-            }elseif ($o == 4){
-                if ($_SESSION['j'][1][0] == '<span class="x">X</span>' || $_SESSION['j'][1][0] == '<span class="o">O</span>'){
-                    jogada_invalida();
-                }elseif ($_SESSION['j'][1][0] == 4) {
-                    $_SESSION['j'][1][0] = '<span class="o">O</span>';
-                    $_SESSION['deu_velha']++;
-                    $_SESSION['x/o'] = 1;
-                }
-            }elseif ($o == 5){
-                if ($_SESSION['j'][1][1] == '<span class="x">X</span>' || $_SESSION['j'][1][1] == '<span class="o">O</span>'){
-                    jogada_invalida();
-                }elseif ($_SESSION['j'][1][1] == 5) {
-                    $_SESSION['j'][1][1] = '<span class="o">O</span>';
-                    $_SESSION['deu_velha']++;
-                    $_SESSION['x/o'] = 1;
-                }
-            }elseif ($o == 6){
-                if ($_SESSION['j'][1][2] == '<span class="x">X</span>' || $_SESSION['j'][1][2] == '<span class="o">O</span>'){
-                    jogada_invalida();
-                }elseif ($_SESSION['j'][1][2] == 6) {
-                    $_SESSION['j'][1][2] = '<span class="o">O</span>';
-                    $_SESSION['deu_velha']++;
-                    $_SESSION['x/o'] = 1;
-                }
-            }elseif ($o == 7){
-                if ($_SESSION['j'][2][0] == '<span class="x">X</span>' || $_SESSION['j'][2][0] == '<span class="o">O</span>'){
-                    jogada_invalida();
-                }elseif ($_SESSION['j'][2][0] == 7) {
-                    $_SESSION['j'][2][0] = '<span class="o">O</span>';
-                    $_SESSION['deu_velha']++;
-                    $_SESSION['x/o'] = 1;
-                }
-            }elseif ($o == 8){
-                if ($_SESSION['j'][2][1] == '<span class="x">X</span>' || $_SESSION['j'][2][1] == '<span class="o">O</span>'){
-                    jogada_invalida();
-                }elseif ($_SESSION['j'][2][1] == 8) {
-                    $_SESSION['j'][2][1] = '<span class="o">O</span>';
-                    $_SESSION['deu_velha']++;
-                    $_SESSION['x/o'] = 1;
-                }
-            }elseif ($o == 9){
-                if ($_SESSION['j'][2][2] == '<span class="x">X</span>' || $_SESSION['j'][2][2] == '<span class="o">O</span>'){
-                    jogada_invalida();
-                }elseif ($_SESSION['j'][2][2] == 9) {
-                    $_SESSION['j'][2][2] = '<span class="o">O</span>';
-                    $_SESSION['deu_velha']++;
-                    $_SESSION['x/o'] = 1;
-                }
-            }//jogadas de 'O'.
-
+            }//jogadas de 'X' e 'O'.
+            jogada_invalida($x, $o);//faz a validação de jogada.
             fim_jogo ();//valida o fim da partida.
             jogadas_x_o();//informa as jogadas de X e O.
         }//jogadas two players
-
-       tabuleiro ();//mostra o tabuleiro.
+        tabuleiro ();//mostra o tabuleiro.
+        reset_game();
     ?>
-
-    <form class="reset" method="post" action="Reset.php">
-        <label>
-            <input type="submit" value="Reset!">
-        </label>
-    </form><!--formulário de reset que encaminha para reset.php-->
-
 </div>
 </body>
 </html>
