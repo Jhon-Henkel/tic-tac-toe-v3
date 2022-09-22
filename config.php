c415f80d4ea751c04e0d04aad49cc8d912165dcf
Jhonatan Henkel <jhowrf@gmail.com>
2022-09-22 07:08:59 -0300

    Update config.php

--- config.php
+++ config.php
@@ -3,6 +3,6 @@
 //database
 const MYSQL_PASS        = 'ty3jADoyCikQh3Cf';
 const MYSQL_USER        = 'jhonhenkel';
-const MYSQL_SERVER      = 'mysql05-farm88.kinghost.net';
+const MYSQL_SERVER      = 'mysql.jhonhenkel.kinghost.net';
 const MYSQL_CHARSET     = 'utf8';
 const MYSQL_DATABASE    = 'jhonhenkel';
